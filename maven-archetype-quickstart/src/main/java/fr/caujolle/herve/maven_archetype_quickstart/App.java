3381cec818f7d3a0b7b7f637510460edafac2ae3
Herve Caujolle <herve.caujolle@gmail.com>
2026-08-11 22:46:39 +0200

    Modification du code

--- maven-archetype-quickstart/src/main/java/fr/caujolle/herve/maven_archetype_quickstart/App.java
+++ maven-archetype-quickstart/src/main/java/fr/caujolle/herve/maven_archetype_quickstart/App.java
@@ -5,6 +5,6 @@ package fr.caujolle.herve.maven_archetype_quickstart;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! dddddddddddddddddddddddddddddddddddddddddddddd");
     }
 }
