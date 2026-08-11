626a4a8063d701c897df6cc82820a2b5f1e45a36
Herve Caujolle <herve.caujolle@gmail.com>
2026-08-11 21:55:42 +0200

    Initial commit

---
+++ maven-archetype-quickstart/src/main/java/fr/caujolle/herve/maven_archetype_quickstart/App.java
@@ -0,0 +1,10 @@
+package fr.caujolle.herve.maven_archetype_quickstart;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
