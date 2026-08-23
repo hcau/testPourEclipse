a4c05a2c6ae6f2cf096af7ddb65fb44b7291ca9b
Herve Caujolle <herve.caujolle@gmail.com>
2026-08-23 19:40:47 +0200

    [TEST][CAS-7348] Ajout de code

---
+++ testEclipse2/src/main/java/fr/caujolle/herve/maven_archetype_quickstart/HCauApp.java
@@ -0,0 +1,22 @@
+package fr.caujolle.herve.maven_archetype_quickstart;
+
+/**
+ * Hello world!
+ */
+public class HCauApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    private void sysout() {
+		// TODO Auto-generated method stub
+    	int toto = 1;
+    	if(toto == 1) {
+    		System.out.println("Ah que coucou!!!!");
+    	}
+
+	}
+    
+    
+    
+}
